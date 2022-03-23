76384f3afdce94571be25362970d9a4390ce151f
lukkerr <luciano_carvalho@outlook.com.br>
2022-03-22 23:53:55 -0300

    entrega de atividade

--- src/br/edu/ifpb/gugawag/so/sockets/Client.java
+++ src/br/edu/ifpb/gugawag/so/sockets/Client.java
@@ -1,18 +1,143 @@
 package br.edu.ifpb.gugawag.so.sockets;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Client {
 
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("localhost", 7000);
-        Scanner in = new Scanner(socket.getInputStream());
-        while(in.hasNextLine()) {
-            System.out.println(in.nextLine());
+        System.out.println("== Cliente ==");
+
+        Socket socket = new Socket("127.0.0.1", 7001);
+        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+        DataInputStream dis = new DataInputStream(socket.getInputStream());
+
+        while (true) {
+            System.out.println("\n---------------------------------------");
+            System.out.println("---------------- Opções ---------------");
+            System.out.println("---------------------------------------");
+            System.out.println("---- create  | criar novo arquivos ----");
+            System.out.println("---- readdir | leitura de arquivos ----");
+            System.out.println("---- rename  | renomear arquivos   ----");
+            System.out.println("---- remove  | remover arquivos    ----");
+            System.out.println("---- exit    | sair de servidor    ----");
+            System.out.println("---------------------------------------\n");
+
+            System.out.print("Digite a opção desejada: ");
+            Scanner teclado = new Scanner(System.in);
+            String mensagem = teclado.nextLine();
+
+            if(mensagem.trim().toLowerCase().matches("readdir")) {
+                System.out.println( enviarMensagem("readdir", null, null, dos, dis) );
+            } else if(mensagem.trim().toLowerCase().matches("rename")) {
+                System.out.println( enviarMensagem("readdirnumber", null, null, dos, dis) );
+                int maxIndex = Integer.parseInt( enviarMensagem("readdirnumberlength", null, null, dos, dis) );
+
+                Scanner archiveNameScanner = new Scanner(System.in);
+                String archiveNumber = "";
+                while(archiveNumber.equals("")) {
+                    System.out.println("Digite o número do arquivo a ser renomeado ou aperte 'x' para sair: ");
+                    archiveNumber = archiveNameScanner.nextLine();
+
+                    if(archiveNumber.trim().matches("x|X")) break;
+
+                    if(archiveNumber.equals("") || (!archiveNumber.equals("") && !archiveNumber.matches("-?\\d+(\\.\\d+)?")) ||
+                            (archiveNumber.matches("-?\\d+(\\.\\d+)?") && Integer.parseInt(archiveNumber) < 1) ||
+                            (archiveNumber.matches("-?\\d+(\\.\\d+)?") && Integer.parseInt(archiveNumber) > maxIndex)) {
+                        System.out.println("Erro ao selecionar número do arquivo, tente novamente.");
+                        archiveNumber = "";
+                    }
+                }
+
+                if(archiveNumber.trim().matches("x|X")) continue;
+
+                Scanner archiveNewNameScanner = new Scanner(System.in);
+                String nameNewArchive = "";
+                while(nameNewArchive.equals("")) {
+                    System.out.println("Digite o nome novo do arquivo: (mínimo 3 caracteres)");
+                    nameNewArchive = archiveNewNameScanner.nextLine();
+                    nameNewArchive = nameNewArchive.trim();
+
+                    if(nameNewArchive.equals("") || !nameNewArchive.equals("") && nameNewArchive.trim().length() < 3) {
+                        System.out.println("Erro ao definir novo tamanho, tente novamente.");
+                        nameNewArchive = "";
+                    }
+                }
+
+                System.out.println( enviarMensagem("rename", archiveNumber, nameNewArchive, dos, dis) );
+
+            } else if(mensagem.trim().toLowerCase().matches("remove")) {
+
+                System.out.println( enviarMensagem("readdirnumber", null, null, dos, dis) );
+                int maxIndex = Integer.parseInt( enviarMensagem("readdirnumberlength", null, null, dos, dis) );
+
+                Scanner archiveNameScanner = new Scanner(System.in);
+                String archiveNumber = "";
+                while(archiveNumber.equals("")) {
+                    System.out.println("Digite o número do arquivo a ser removido ou aperte 'x' para sair: ");
+                    archiveNumber = archiveNameScanner.nextLine();
+                    if(archiveNumber.trim().matches("x|X")) break;
+
+                    if(archiveNumber.equals("") || (!archiveNumber.equals("") && !archiveNumber.matches("-?\\d+(\\.\\d+)?")) ||
+                            (archiveNumber.matches("-?\\d+(\\.\\d+)?") && Integer.parseInt(archiveNumber) < 1) ||
+                            (archiveNumber.matches("-?\\d+(\\.\\d+)?") && Integer.parseInt(archiveNumber) > maxIndex)) {
+                        System.out.println("Erro ao selecionar número do arquivo, tente novamente.");
+                        archiveNumber = "";
+                    }
+                }
+
+                if(archiveNumber.trim().matches("x|X")) continue;
+                System.out.println( enviarMensagem("remove", archiveNumber, null, dos, dis) );
+
+            } else if(mensagem.trim().toLowerCase().matches("create")) {
+
+                Scanner archiveNewNameScanner = new Scanner(System.in);
+                String nameNewArchive = "";
+                while(nameNewArchive.equals("")) {
+                    System.out.println("Digite o nome do novo arquivo: (mínimo 3 caracteres)");
+                    nameNewArchive = archiveNewNameScanner.nextLine();
+                    nameNewArchive = nameNewArchive.trim();
+
+                    if(nameNewArchive.equals("") || (!nameNewArchive.equals("") && nameNewArchive.trim().length() < 3)) {
+                        System.out.println("Erro ao definir novo tamanho, tente novamente.");
+                        nameNewArchive = "";
+                    }
+                }
+
+                System.out.println( enviarMensagem("create", null, nameNewArchive, dos, dis) );
+            } else if(mensagem.trim().toLowerCase().matches("exit")) {
+                socket.close();
+                break;
+            } else
+                System.out.println("Comando Invalido tente novamente");
         }
-        in.close();
-        socket.close();
+    }
+
+    public static String formatarComando(String comando, String number, String novoNome) {
+
+        String result = null;
+
+        if( comando != null && !comando.trim().equals("") &&
+                comando.trim().toLowerCase().matches("create|readdir|readdirnumber|readdirnumberlength|rename|remove") ) {
+            if(comando.equals("rename"))
+                result = comando + "|" + number + "|" + novoNome;
+            else if(comando.equals("create"))
+                result = comando + "|" + novoNome;
+            else if(comando.equals("remove"))
+                result = comando + "|" + number;
+            else
+                result = comando;
+        }
+
+        return result;
+    }
+
+    public static String enviarMensagem(String comando, String indexComando, String novoNumber, DataOutputStream dos, DataInputStream dis) throws IOException {
+        dos.writeUTF(formatarComando(comando, indexComando, novoNumber));
+        return dis.readUTF();
     }
 }
--- src/br/edu/ifpb/gugawag/so/sockets/Server.java
+++ src/br/edu/ifpb/gugawag/so/sockets/Server.java
@@ -1,23 +1,123 @@
 package br.edu.ifpb.gugawag.so.sockets;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
 public class Server {
+
+    private static String HOME = System.getProperty("user.home");
+
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(7000);
-        System.out.println("==Servidor==");
-        while(true) {
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Cliente " + clientSocket.getInetAddress() + ":" + clientSocket.getPort());
+        System.out.println("== Servidor ==");
 
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
-            out.println("Tchau!");
-            out.close();
-            clientSocket.close();
+        ServerSocket serverSocket = new ServerSocket(7001);
+        Socket socket = serverSocket.accept();
+
+        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+        DataInputStream dis = new DataInputStream(socket.getInputStream());
+
+        while (true) {
+            System.out.println("Cliente: " + socket.getInetAddress());
+
+            String mensagem = dis.readUTF();
+            System.out.println(mensagem);
+            String response = "";
+
+            if( mensagem != null && !mensagem.trim().equals("") ) {
+                File home = new File(HOME);
+
+                switch (mensagem.trim().split("\\|")[0].toLowerCase(Locale.ROOT)) {
+                    case "readdir": {
+                        response = filesInFolder(home);
+                        break;
+                    }
+                    case "readdirnumber": {
+                        List<String> listFiles = renameFilesInFolder(home);
+                        int lengthMaxOfNumber = String.valueOf(listFiles.size()).length();
+                        String spaces = new String(new char[lengthMaxOfNumber]).replace("\0", " ");
+
+                        for(int i = 0; i < listFiles.size(); i++)
+                            response += (i+1) + "|" + spaces + listFiles.get(i) + "\n";
+
+                        break;
+                    }
+                    case "readdirnumberlength": {
+                        response = String.valueOf( renameFilesInFolder(home).size() );
+                        break;
+                    }
+                    case "rename": {
+                        List<String> commandsElement = Arrays.stream(mensagem.split("\\|")).toList();
+                        int indexFile = Integer.parseInt( commandsElement.get(1) );
+                        String newName = commandsElement.get(2);
+
+                        List<String> listFiles = renameFilesInFolder(home);
+                        File oldFileName = new File(HOME + "\\" + listFiles.get(indexFile-1));
+                        File newFileName = new File(HOME + "\\" + newName);
+                        if( !newFileName.exists() ) {
+                            oldFileName.renameTo(newFileName);
+                            response = "Arquivo renomeado com sucesso!";
+                        } else
+                            response = "Erro: Novo nome já existente";
+
+                        break;
+                    }
+                    case "remove": {
+                        List<String> listFiles = renameFilesInFolder(home);
+                        List<String> commandsElement = Arrays.stream(mensagem.split("\\|")).toList();
+                        int indexFileChange = Integer.parseInt( commandsElement.get(1) );
+
+                        File oldFile = new File(HOME + "\\" + listFiles.get(indexFileChange-1));
+                        oldFile.delete();
+
+                        response = "Arquivo removido com sucesso!";
+                        break;
+                    }
+                    case "create": {
+                        List<String> commandsElement = Arrays.stream(mensagem.split("\\|")).toList();
+                        String newFileName = commandsElement.get(1);
+
+                        File newFile = new File(HOME + "\\" + newFileName);
+                        if( !newFile.exists() ) {
+                            newFile.createNewFile();
+                            response = "Arquivo criado com sucesso!";
+                        } else
+                            response = "Erro: Novo arquivo já existente, tente novamente";
+
+                        break;
+                    }
+                    default:
+                        response = "Erro: Comando Invalido";
+                }
+
+                dos.writeUTF(response);
+            } else
+                dos.writeUTF("Mensagem Invalida: " + mensagem);
+
         }
+    }
 
+    public static String filesInFolder(final File folder) {
+        StringBuilder response = new StringBuilder();
+        for (final File fileOrFolder : folder.listFiles()) {
+            response.append( fileOrFolder.isDirectory() ? "DIR| " + fileOrFolder : "FIL| " + fileOrFolder.getName());
+            response.append("\n");
+        }
+        return response.substring(0, response.length() - 3);
+    }
+
+    public static List<String> renameFilesInFolder(final File folder) {
+        List<String> response = new ArrayList<>();
+        for (final File fileOrFolder : folder.listFiles())
+            if (!fileOrFolder.isDirectory()) response.add(fileOrFolder.getName());
+
+        return response;
     }
 }
